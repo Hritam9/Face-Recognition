dbb58537493e6cf932268382c78df4b75820b1d6
Hritam Jaiwal <43675990+Hritam9@users.noreply.github.com>
2022-02-10 19:00:03 +0530

    Update face_rec.py

--- face_rec.py
+++ face_rec.py
@@ -6,7 +6,11 @@ import numpy as np
 from time import sleep
 
 def get_encoded_faces():
-
+    """
+    looks through the faces folder and encodes all
+    the faces
+    :return: dict of (name, image encoded)
+    """
     encoded = {}
 
     for dirpath, dnames, fnames in os.walk("./faces"):
@@ -19,14 +23,21 @@ def get_encoded_faces():
     return encoded
 
 def unknown_image_encoded(img):
-
+     """
+    encode a face given the file name
+    """
     face = fr.load_image_file("faces/"+img)
     encoding = fr.face_encodings(face)[0]
 
     return encoding
 
 def classify_face(im):
-
+     """
+    will find all of the faces in a given image and label
+    them if it knows what they are
+    :param im: str of file path
+    :return: list of face names
+    """
     faces = get_encoded_faces()
     faces_encoded = list(faces.values())
     known_face_names = list(faces.keys())
@@ -41,7 +52,8 @@ def classify_face(im):
         #See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(faces_encoded, face_encoding)
         name = "Unknown"
-
+        
+         # use the known face with the smallest distance to the new face
         face_distances = face_recognition.face_distance(faces_encoded, face_encoding)
         best_match_index = np.argmin(face_distances)
         if matches[best_match_index]:
